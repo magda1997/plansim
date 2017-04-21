13a0581bcf2a3b33fe0f62a8ab453802ea801693
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2017-04-21 17:10:01 +0200

    Alokowanie pamięci

--- lbody.c
+++ lbody.c
@@ -1,6 +1,9 @@
 #include "lbody.h"
 
+#include "body.h"
+
 #include <stdlib.h>
+#include <stdio.h>
 
 void lbody_initialize(lbody_t *list)
 {
@@ -13,3 +16,25 @@ void lbody_initialize(lbody_t *list)
     list->body_count = 0;
     list->array_size = DEFAULT_ARRAY_SIZE;
 }
+
+static void lbody_extend(lbody_t *list)
+{
+    body_t *bodies = realloc(list->bodies, list->array_size * 2 * sizeof *bodies);
+
+    if (bodies == NULL) {
+        fprintf(stderr, "Rozszerzenie tablicy nie powiodlo sie.\n");
+        return;
+    }
+
+    list->bodies = bodies;
+    list->array_size *= 2;
+}
+
+/*void lbody_add(body_t *body, lbody_t *list)
+{
+    if (list->body_count == list->array_size) {
+        lbody_extend(list);
+    }
+
+    list->bodies[list->body_count++] = body;
+}*/
