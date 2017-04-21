a9a48cf201343d49dcc18edd900067f1f1fd78fc
Magdalena <karolczukmagda@wp.pl>
2017-04-21 16:28:11 +0200

    Wstępna wersja wczytywania danych o ciałach z pliku

--- lbody.c
+++ lbody.c
@@ -1,5 +1,8 @@
 #include "lbody.h"
 
+#include "body.h"
+
+#include <stdio.h>
 #include <stdlib.h>
 
 void lbody_initialize(lbody_t *list)
@@ -12,4 +15,33 @@ void lbody_initialize(lbody_t *list)
 
     list->body_count = 0;
     list->array_size = DEFAULT_ARRAY_SIZE;
+}
+
+void lbody_read(lbody_t *list, FILE *in)
+{
+    char name[256];
+    double mass;
+    double x,y,z;
+    double vx,vy,vz;
+
+	while (fscanf(in, "%s %lf %lf %lf %lf %lf %lf %lf", name, &mass, &x, &y, &z, &vx, &vy, &vz) == 8) {
+	    if (mass < 0) {
+	        fprintf(stderr, "Masa nie moze byc ujemna, cialo pominiete.\n");
+	        continue;
+        }
+        
+        body_t *body = body_create(name);
+        
+        if (body != NULL) {
+            body->mass = mass;
+            body->x = x;
+            body->y = y;
+            body->z = z;
+            body->vx = vx;
+            body->vy = vy;
+            body->vz = vz;
+            
+            lbody_add(body, list);
+        }   
+    }
 }
